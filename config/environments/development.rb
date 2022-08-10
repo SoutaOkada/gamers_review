c862e47d264329fb9682485352e15e14acfa44af
EC2 Default User <ec2-user@ip-172-31-46-197.ap-northeast-1.compute.internal>
2022-08-10 06:35:41 +0000

    [update] allow host authority
    edit config/enviromnet/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,3 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
+
+config.hosts << "46b11eb46bd04857963414b2d7720b4a.vfs.cloud9.ap-northeast-1.amazonaws.com"
